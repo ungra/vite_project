ed0ec3b66ac66a0c66cda5dee237c2fa3c645ce0
ungra <junhee_lee2@tmax.co.kr>
2024-11-14 21:14:50 +0900

    delete tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./Timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -27,12 +30,37 @@ const Photo = styled.img`
   border-radius: 15px;
 `;
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteBtn = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("Are you sure you want delete this tweet?");
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, "tweet", id));
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>
+        ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
